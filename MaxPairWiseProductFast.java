f78f5efffe836916ca736439b3180cdd521be91f
Amit mishra <amitmishra0617@gmail.com>
2021-12-29 22:47:43 +0530

    jorrdar bugfix

--- MaxPairWiseProductFast.java
+++ MaxPairWiseProductFast.java
@@ -10,7 +10,12 @@ ind1 = i ;
 }
 }
 int ind2 = 0;
-if(ind1 == 0) {ind2 = 1;}
+if(ind1 == 0) {ind2 = 1;} // Why? 
+//the following if conditional will never get satisfied if there 
+//are only two elements in array and array have first largest 
+//number at first index (ind1 = 0) and hence it ind2 will not get 
+//updated and will remain ind2=0 which is not second largest 
+//number but the first largest number.
 else ind2 = 0;
 for (int j=1; j<n; j++){
 if(numbers[j]!=numbers[ind1] && numbers[j]>numbers[ind2]){
--- OpMaxPairWiseProduct.java
+++ OpMaxPairWiseProduct.java
@@ -1,48 +1,52 @@
 import java.util.Scanner;
 import java.util.List;
 import java.util.Arrays;
-import java.util.Collections;
 
 public class OpMaxPairWiseProduct{
-
-static long getOpMaxPairWiseProduct(long[] numbers){
-int num = numbers.length;
+	static long[] swap(long[] numbers, int a, int b){
+		long temp = numbers[a];
+		numbers[a] = numbers[b];
+		numbers[b] = temp;
+		System.out.println(a+","+b);
+		System.out.println(Arrays.toString(numbers));
+		return numbers;
+	}
+	static long getOpMaxPairWiseProduct(long[] numbers){
+		int num = numbers.length;
 //Basically in this algorithm we want to ignore the largest 
 //number we got the first time and then scan only among the remaining
 //elements to get another largest among them even if it is same to
 //previous largest number.
-int index = 0;
-for(int i=0; i<num; i++){
-if(numbers[i]>numbers[index]){
-index = i;
-}}
+		int index = 0;
+		for(int i=0; i<num; i++){
+		if(numbers[i]>numbers[index]){
+		index = i;
+		}}
 //Now we have the position of largest number now we are going to 
-//push it to last index position. Why? Keep watching.
-List<long []> list = Arrays.asList(numbers);
-Collections.swap(list, index, num-1);
-index = 0;
-for(int i = 0; i<num -1; i++){	//Here is the reason after pushing 
-					//largest number to the terminal end
-					//we can easily ignore that number 
-if(numbers[i]>numbers[index]){
-index = i;
-}
-}
-Collections.swap(list, index, num-2);
+//push it to last index position. Why?
+		swap(numbers, index, num-1);
+		index = 0;
+		for(int i = 0; i<num -1; i++){	//Here is the reason after pushing 
+							//largest number to the terminal end
+							//we can easily ignore that number 
+			if(numbers[i]>numbers[index]){
+				index = i;
+			}
+		}
+		swap(numbers, index, num-2);
 //Now the two largest numbers are at last two position
 //we can now find max product pair
-return numbers[num-1]*numbers[num-2];
-}
-
+		return numbers[num-1]*numbers[num-2];
+	}
 
 public static void main(String args[]){
-Scanner s = new Scanner(System.in);
-var num = s.nextInt();
-long[] numbers = new long[num];
-for (int i=0; i<num; i++){
-numbers[i]=s.nextLong();
-}
-System.out.println(getOpMaxPairWiseProduct(numbers));
+	Scanner s = new Scanner(System.in);
+	var num = s.nextInt();
+	long[] numbers = new long[num];
+	for (int i=0; i<num; i++){
+		numbers[i]=s.nextLong();
+	}
+	System.out.println(getOpMaxPairWiseProduct(numbers));
 }
 
 }
