43ae1b4839a6b8c56f00824001c036711770c449
Amit mishra <amitmishra0617@gmail.com>
2022-01-04 22:25:45 +0530

    updated

--- GCD.java
+++ GCD.java
@@ -2,38 +2,14 @@ import java.util.*;
 import java.io.*;
 
 public class GCD{
-
-/*static int[] swap(int n1, int n2){
-int temp = n1;
-n1 = n2;
-n2 = temp;
-int[] numbers = new int[2];
-numbers[0] = n1;
-numbers[1] = n2;
-return numbers;
-}
-*/
-
 public static void main(String[] args) {
 	Scanner s = new Scanner(System.in);
 	int n1 = s.nextInt();
 	int n2 = s.nextInt();
 	int d = 0;
-	/*if (n1<n2){
-	int[] arr = new int[2];
-	arr[0] = n1;
-	arr[1] = n2;
-	swap(n1, n2);
-	System.out.println(n1+","+n2);
-	}*/
-	for (int i = 1; i<=n1; i++){
-	if (n1%i==0 && n2%i==0){
-	if (n1 ==0){return n2;}
-	if (n2 ==0 ) { 
-	   return n1;
-	}
-	d = i;
-	}
+	int min = Math.min(n1,n2);
+	for (int i = 1; i<=min; i++){
+		d = i;
 	}
 	System.out.println(d);
 }
--- GCDFast.java
+++ GCDFast.java
@@ -2,21 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class GCDFast{
-static void swap (int n1, int n2){
-
-}
-static void getGCD(int n1, int n2){
-if (n1%n2 == 0){
-return n2;
-}
-else {
-while (n2 != 0){
-int temp = n2;
-n2 = n1%n2;
-n1 = temp;
-}
-} 
-}
 public static void main(String[] args){
 Scanner s = new Scanner(System.in);
 int n1 = s.nextInt();
@@ -25,5 +10,25 @@ if (n2 > n1){
 swap(n1, n2);
 }
 System.out.println(getGCD(n1, n2));
+}
+static int[] swap (int n1, int n2){
+int[] arr = new int[2];
+arr[0] = n1 - n2;
+arr[1] = arr[0] + n2;
+arr[0] = arr[1] - arr[0];
+return arr;
+}
+static int getGCD(int n1, int n2){
+if (n1%n2 == 0){
+return n2;
+}
+else {
+while (n2 != 0){
+int temp = n2; //storing divisor
+n2 = n1%n2; //updating remainder which will be used as divisor
+n1 = temp; //updating with divisor which will be used as dividend
+}
+return n1; //returning divisor when n2 i.e., remainder is zero
+} 
 }
 }
